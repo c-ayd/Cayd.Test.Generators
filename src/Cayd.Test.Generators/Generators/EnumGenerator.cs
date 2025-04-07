5a2f444d628032691ace95d64eaf05a5a53e0dfe
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-07 21:23:11 +0200

    add enum generator

---
+++ src/Cayd.Test.Generators/Generators/EnumGenerator.cs
@@ -0,0 +1,93 @@
+﻿namespace Cayd.Test.Generators.Generators
+{
+    public static class EnumGenerator
+    {
+        /// <summary>
+        /// Generates a random enum value from the desired type.
+        /// </summary>
+        /// <typeparam name="T">Type of enum.</typeparam>
+        /// <param name="exclude">Enum values to exclude.</param>
+        /// <returns>Returns a random enum value from the desired type.</returns>
+        public static T Generate<T>(params T[] exclude)
+            where T : Enum
+        {
+            var values = GetEnumValues(exclude);
+            return values[Random.Shared.Next(values.Count())];
+        }
+
+        /// <summary>
+        /// Generates a random enum value within a desired range.
+        /// </summary>
+        /// <typeparam name="T">Type of enum.</typeparam>
+        /// <param name="min">Minimum numerical value of the enum (inclusive).</param>
+        /// <param name="max">Maximum numerical value of the enum (inclusive). It must be greater than the minimum value.</param>
+        /// <param name="exclude">Enum values to exclude.</param>
+        /// <returns>Returns a random enum value within the desired range.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static T GenerateBetween<T>(T min, T max, params T[] exclude)
+            where T : Enum
+        {
+            var values = GetEnumValues(exclude);
+
+            if (!values.Contains(min))
+                throw new ArgumentOutOfRangeException(nameof(min), min.ToString(), $"{min.ToString()} does not exist in enum {typeof(T).Name}");
+            if (!values.Contains(max))
+                throw new ArgumentOutOfRangeException(nameof(max), max.ToString(), $"{max.ToString()} does not exist in enum {typeof(T).Name}");
+            if (max.CompareTo(min) < 0)
+                throw new ArgumentOutOfRangeException(nameof(max), max, $"Maximum value must be greater than the passed minimum value: {min}");
+
+            int minIndex = values.FindIndex(0, values.Count, e => e.Equals(min));
+            int maxIndex = values.FindIndex(0, values.Count, e => e.Equals(max));
+            return values[Random.Shared.Next(minIndex, maxIndex)];
+        }
+
+        /// <summary>
+        /// Generates a random enum value within a desired range starting from a specific value.
+        /// </summary>
+        /// <typeparam name="T">Type of enum.</typeparam>
+        /// <param name="start">Numerical starting value of the enum (inclusive).</param>
+        /// <param name="direction">Direction of the range (numerical).</param>
+        /// <param name="exclude">Enum values to exclude.</param>
+        /// <returns>Returns a random enum value within the desired range.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static T GenerateStartingFrom<T>(T start, EDirection direction, params T[] exclude)
+            where T : Enum
+        {
+            var values = GetEnumValues(exclude);
+
+            if (!values.Contains(start))
+                throw new ArgumentOutOfRangeException(nameof(start), start.ToString(), $"{start.ToString()} does not exist in enum {typeof(T).Name}");
+
+            int startIndex = values.FindIndex(0, values.Count, e => e.Equals(start));
+            switch (direction)
+            {
+                case EDirection.Up:
+                    return values[Random.Shared.Next(startIndex, values.Count)];
+                default:
+                    if (startIndex == values.Count - 1)
+                        return values[Random.Shared.Next(0, values.Count)];
+
+                    return values[Random.Shared.Next(0, startIndex + 1)];
+            }
+        }
+
+        private static List<T> GetEnumValues<T>(params T[] exclude)
+            where T : Enum
+        {
+            var values = Enum.GetValues(typeof(T)).OfType<T>().ToList();
+            if (exclude.Length > 0)
+            {
+                values.RemoveAll(e => exclude.Contains(e));
+            }
+
+            values.Sort();
+            return values;
+        }
+
+        public enum EDirection
+        {
+            Up,
+            Down
+        }
+    }
+}
---
+++ test/UnitTests/Cayd.Test.Generators.Test.Unit/Generators/EnumGeneratorTest.cs
@@ -0,0 +1,152 @@
+﻿using Cayd.Test.Generators.Generators;
+using System.Reflection;
+
+namespace Cayd.Test.Generators.Test.Unit.Generators
+{
+    public class EnumGeneratorTest
+    {
+        public enum ETestEnum
+        {
+            Undefined   =   -1,
+            None        =   0,
+            State1      =   1,
+            State2      =   2,
+            State3      =   3,
+            State4      =   100,
+            State5      =   200,
+            State6      =   1000
+        }
+
+        [Fact]
+        public void GetEnumValues_WithoutExclusion_ShouldReturnAllValuesOfEnum()
+        {
+            // Arrange
+            var method = typeof(EnumGenerator).GetMethod("GetEnumValues", BindingFlags.Static | BindingFlags.NonPublic)!.MakeGenericMethod(typeof(ETestEnum));
+            var values = Enum.GetValues(typeof(ETestEnum)).OfType<ETestEnum>().ToList();
+            values.Sort();
+
+            // Act
+            var result = (List<ETestEnum>)method.Invoke(null, new object[] { Array.Empty<ETestEnum>() })!;
+
+            // Arrange
+            Assert.NotNull(result);
+            Assert.True(result.Count == 8, $"Count: {result.Count}. Expected: 8");
+            Assert.True(values.SequenceEqual(result), $"The values or the order of the list is not the same");
+        }
+
+        [Fact]
+        public void GetEnumValues_WithExclusion_ShouldReturnOnlyDesiredValuesOfEnum()
+        {
+            // Arrange
+            var method = typeof(EnumGenerator).GetMethod("GetEnumValues", BindingFlags.Static | BindingFlags.NonPublic)!.MakeGenericMethod(typeof(ETestEnum));
+            var values = new List<ETestEnum>() { ETestEnum.Undefined, ETestEnum.None, ETestEnum.State1, ETestEnum.State5, ETestEnum.State6 };
+
+            // Act
+            var result = (List<ETestEnum>)method.Invoke(null, new object[] { new ETestEnum[] { ETestEnum.State2, ETestEnum.State3, ETestEnum.State4 } })!;
+
+            // Arrange
+            Assert.NotNull(result);
+            Assert.True(result.Count == 5, $"Count: {result.Count}. Expected: 5");
+            Assert.True(values.SequenceEqual(result), $"The values or the order of the list is not the same");
+        }
+
+        [Fact]
+        public void Generate_ShouldReturnValueWithinRangeWithSameType()
+        {
+            // Arrange
+            var values = Enum.GetValues(typeof(ETestEnum)).OfType<ETestEnum>().ToList();
+            values.Sort();
+
+            // Act
+            var result = EnumGenerator.Generate<ETestEnum>();
+
+            // Assert
+            Assert.IsType<ETestEnum>(result);
+            Assert.Contains(result, values);
+        }
+
+        [Fact]
+        public void GenerateBetween_ShouldReturnValueWithinRangeWithSameType()
+        {
+            // Arrange
+            var values = Enum.GetValues(typeof(ETestEnum)).OfType<ETestEnum>().ToList();
+            values.Sort();
+            var min = ETestEnum.State2;
+            var max = ETestEnum.State5;
+
+            // Act
+            var result = EnumGenerator.GenerateBetween(min, max);
+
+            // Assert
+            Assert.IsType<ETestEnum>(result);
+            Assert.Contains(result, values);
+            Assert.True(result >= min, $"Returned Value: {result}, Min Value: {min}");
+            Assert.True(result <= max, $"Returned Value: {result}, Max Value: {max}");
+        }
+
+        [Fact]
+        public void GenerateBetween_WhenRangeIsNotCorrect_ShouldThrowArgumentOutOfRangeException()
+        {
+            // Act
+            var result = Record.Exception(() =>
+            {
+                EnumGenerator.GenerateBetween(ETestEnum.State6, ETestEnum.State1);
+            });
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<ArgumentOutOfRangeException>(result);
+        }
+
+        [Fact]
+        public void GenerateStartingFrom_WhenDirectionIsUp_ShouldReturnValueWithinRangeWithSameType()
+        {
+            // Arrange
+            var values = Enum.GetValues(typeof(ETestEnum)).OfType<ETestEnum>().ToList();
+            values.Sort();
+            var start = ETestEnum.State4;
+
+            // Act
+            var result = EnumGenerator.GenerateStartingFrom(start, EnumGenerator.EDirection.Up);
+
+            // Assert
+            Assert.IsType<ETestEnum>(result);
+            Assert.Contains(result, values);
+            Assert.True(result >= start, $"Returned Value: {result}, Start Value: {start}");
+        }
+
+        [Fact]
+        public void GenerateStartingFrom_WhenDirectionIsDown_ShouldReturnValueWithinRangeWithSameType()
+        {
+            // Arrange
+            var values = Enum.GetValues(typeof(ETestEnum)).OfType<ETestEnum>().ToList();
+            values.Sort();
+            var start = ETestEnum.State4;
+
+            // Act
+            var result = EnumGenerator.GenerateStartingFrom(start, EnumGenerator.EDirection.Down);
+
+            // Assert
+            Assert.IsType<ETestEnum>(result);
+            Assert.Contains(result, values);
+            Assert.True(result <= start, $"Returned Value: {result}, Start Value: {start}");
+        }
+
+        [Fact]
+        public void GenerateStartingFrom_WhenDirectionIsDownAndStartingIsLastValue_ShouldReturnValueWithinRangeWithSameType()
+        {
+            // Arrange
+            var values = Enum.GetValues(typeof(ETestEnum)).OfType<ETestEnum>().ToList();
+            values.Sort();
+            var start = ETestEnum.State6;
+
+            // Act
+            var result = EnumGenerator.GenerateStartingFrom(start, EnumGenerator.EDirection.Up);
+
+            // Assert
+            Assert.IsType<ETestEnum>(result);
+            Assert.Contains(result, values);
+            Assert.True(result <= start, $"Returned Value: {result}, Start Value: {start}");
+        }
+    }
+}
