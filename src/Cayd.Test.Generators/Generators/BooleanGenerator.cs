97d5aab0d835a71c85a77c100fc7355fffab3ccb
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-07 14:44:17 +0200

    add boolean generator
    - A lerp math function is added
    - A unit test is added for the project

---
+++ src/Cayd.Test.Generators/Generators/BooleanGenerator.cs
@@ -0,0 +1,28 @@
+﻿namespace Cayd.Test.Generators.Generators
+{
+    public static class BooleanGenerator
+    {
+        /// <summary>
+        /// Generates a random boolean with a proability of 0.5.
+        /// </summary>
+        /// <returns>Returns TRUE or FALSE.</returns>
+        public static bool Generate()
+        {
+            return Random.Shared.Next(0, 2) == 1;
+        }
+
+        /// <summary>
+        /// Generates a random boolean with a desired probability.
+        /// </summary>
+        /// <param name="probability">Probability of the return type to be TRUE. It must be between 0.0 and 1.0</param>
+        /// <returns>Returns TRUE or FALSE.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static bool GenerateWithProbability(double probability)
+        {
+            if (probability < 0.0 || probability > 1.0)
+                throw new ArgumentOutOfRangeException(nameof(probability), probability, "Probability must be between 0.0 and 1.0");
+
+            return Random.Shared.NextDouble() >= probability;
+        }
+    }
+}
---
+++ test/UnitTests/Cayd.Test.Generators.Test.Unit/Generators/BooleanGeneratorTest.cs
@@ -0,0 +1,46 @@
+﻿using Cayd.Test.Generators.Generators;
+
+namespace Cayd.Test.Generators.Test.Unit.Generators
+{
+    public class BooleanGeneratorTest
+    {
+        [Fact]
+        public void Generate_ShouldReturnBoolean()
+        {
+            // Act
+            var result = BooleanGenerator.Generate();
+
+            // Assert
+            Assert.IsType<bool>(result);
+        }
+
+        [Theory]
+        [InlineData(0.1)]
+        [InlineData(0.0)]
+        [InlineData(1.0)]
+        public void GenerateWithProbability_WhenProbabilityIsWithinRange_ShouldReturnBoolean(double probability)
+        {
+            // Act
+            var result = BooleanGenerator.GenerateWithProbability(probability);
+
+            // Assert
+            Assert.IsType<bool>(result);
+        }
+
+        [Theory]
+        [InlineData(-0.1)]
+        [InlineData(1.1)]
+        public void GenerateWithProbability_WhenProbabilityIsNotWithinRange_ShouldThrowArgumentOutOfRangeException(double probability)
+        {
+            // Act
+            var result = Record.Exception(() =>
+            {
+                BooleanGenerator.GenerateWithProbability(probability);
+            });
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<ArgumentOutOfRangeException>(result);
+        }
+    }
+}
