0f642ca8aaa7149611e49f736268daaa40b314dd
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-08 14:45:31 +0200

    add datetime generator

---
+++ src/Cayd.Test.Generators/Generators/DateTimeGenerator.cs
@@ -0,0 +1,71 @@
+﻿namespace Cayd.Test.Generators
+{
+    public static class DateTimeGenerator
+    {
+        /// <summary>
+        /// Generates the current time based on the time zone.
+        /// </summary>
+        /// <param name="timeZone">Time zone of the generated date.</param>
+        /// <returns>Returns a date time.</returns>
+        public static DateTime GenerateNow(ETimeZone timeZone)
+        {
+            switch (timeZone)
+            {
+                case ETimeZone.UTC:
+                    return DateTime.UtcNow;
+                default:
+                    return DateTime.Now;
+            }
+        }
+
+        /// <summary>
+        /// Generates a date time earlier than the current time based on the time zone.
+        /// </summary>
+        /// <param name="timeZone">Time zone of the genertaed date.</param>
+        /// <param name="timeShift">How much earlier the generated date is.</param>
+        /// <returns>Returns a date time.</returns>
+        public static DateTime GenerateBefore(ETimeZone timeZone, TimeSpan timeShift)
+        {
+            DateTime dateTime;
+            switch (timeZone)
+            {
+                case ETimeZone.UTC:
+                    dateTime = DateTime.UtcNow;
+                    break;
+                default:
+                    dateTime = DateTime.Now;
+                    break;
+            }
+
+            return dateTime.Add(-timeShift);
+        }
+
+        /// <summary>
+        /// Generates a date time later than the current time based on the time zone.
+        /// </summary>
+        /// <param name="timeZone">Time zone of the generated date.</param>
+        /// <param name="timeShift">How much late the generated date is.</param>
+        /// <returns>Returns a date time.</returns>
+        public static DateTime GenerateAfter(ETimeZone timeZone, TimeSpan timeShift)
+        {
+            DateTime dateTime;
+            switch (timeZone)
+            {
+                case ETimeZone.UTC:
+                    dateTime = DateTime.UtcNow;
+                    break;
+                default:
+                    dateTime = DateTime.Now;
+                    break;
+            }
+
+            return dateTime.Add(timeShift);
+        }
+
+        public enum ETimeZone
+        {
+            UTC         =   0,
+            Local       =   1
+        }
+    }
+}
---
+++ test/UnitTests/Cayd.Test.Generators.Test.Unit/Generators/DateTimeGeneratorTest.cs
@@ -0,0 +1,75 @@
+﻿namespace Cayd.Test.Generators.Test.Unit.Generators
+{
+    public class DateTimeGeneratorTest
+    {
+        [Fact]
+        public void GenerateNow_WhenTimeZoneIsUtc_ShouldReturnUtcNow()
+        {
+            // Act
+            var result = DateTimeGenerator.GenerateNow(DateTimeGenerator.ETimeZone.UTC);
+
+            // Assert
+            var difference = (result - DateTime.UtcNow);
+            Assert.True(difference.TotalSeconds <= 5, "Result is later than UTC now by more than 5 seconds");
+        }
+
+        [Fact]
+        public void GenerateNow_WhenTimeZoneIsLocal_ShouldReturnLocalNow()
+        {
+            // Act
+            var result = DateTimeGenerator.GenerateNow(DateTimeGenerator.ETimeZone.Local);
+
+            // Assert
+            var difference = (result - DateTime.Now);
+            Assert.True(difference.TotalSeconds <= 5, "Result is later than local now by more than 5 seconds");
+        }
+
+        [Fact]
+        public void GenerateBefore_WhenTimeZoneIsUtcAndOneDayShiftIsGiven_ShouldReturnPastTimeAccordingToUtc()
+        {
+            // Act
+            var result = DateTimeGenerator.GenerateBefore(DateTimeGenerator.ETimeZone.UTC, TimeSpan.FromDays(1));
+
+            // Assert
+            var difference = (DateTime.UtcNow - result);
+            Assert.True(DateTime.UtcNow.CompareTo(result) > 0, "Result is not earlier than UTC now");
+            Assert.True(difference.TotalSeconds > 86399 && difference.TotalSeconds < 86405, "There is not a day difference between the two dates.");
+        }
+
+        [Fact]
+        public void GenerateBefore_WhenTimeZoneIsLocalAndOneDayShiftIsGiven_ShouldReturnPastTimeAccordingToUtc()
+        {
+            // Act
+            var result = DateTimeGenerator.GenerateBefore(DateTimeGenerator.ETimeZone.Local, TimeSpan.FromDays(1));
+
+            // Assert
+            var difference = (DateTime.Now - result);
+            Assert.True(DateTime.Now.CompareTo(result) > 0, "Result is not earlier than UTC now");
+            Assert.True(difference.TotalSeconds > 86399 && difference.TotalSeconds < 86405, "There is not a day difference between the two dates.");
+        }
+
+        [Fact]
+        public void GenerateAfter_WhenTimeZoneIsUtcAndOneDayShiftIsGiven_ShouldReturnPastTimeAccordingToUtc()
+        {
+            // Act
+            var result = DateTimeGenerator.GenerateAfter(DateTimeGenerator.ETimeZone.UTC, TimeSpan.FromDays(1));
+
+            // Assert
+            var difference = (result - DateTime.UtcNow);
+            Assert.True(DateTime.UtcNow.CompareTo(result) < 0, "Result is not later than UTC now");
+            Assert.True(difference.TotalSeconds > 86399 && difference.TotalSeconds < 86405, "There is not a day difference between the two dates.");
+        }
+
+        [Fact]
+        public void GenerateAfter_WhenTimeZoneIsLocalAndOneDayShiftIsGiven_ShouldReturnPastTimeAccordingToUtc()
+        {
+            // Act
+            var result = DateTimeGenerator.GenerateAfter(DateTimeGenerator.ETimeZone.Local, TimeSpan.FromDays(1));
+
+            // Assert
+            var difference = (result - DateTime.Now);
+            Assert.True(DateTime.Now.CompareTo(result) < 0, "Result is not later than UTC now");
+            Assert.True(difference.TotalSeconds > 86399 && difference.TotalSeconds < 86405, "There is not a day difference between the two dates.");
+        }
+    }
+}
