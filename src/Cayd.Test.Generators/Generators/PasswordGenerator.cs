f96c34664fee241f67ad304aa7529c622c86518a
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-08 22:51:09 +0200

    add password generator

---
+++ src/Cayd.Test.Generators/Generators/PasswordGenerator.cs
@@ -0,0 +1,142 @@
+﻿using System.Text;
+
+namespace Cayd.Test.Generators
+{
+    public static class PasswordGenerator
+    {
+        /// <summary>
+        /// Generates a random password.
+        /// </summary>
+        /// <param name="length">Length of the password. The default is 6.</param>
+        /// <returns>Returns a random password.</returns>
+        public static string Generate(int length = 6)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < length; ++i)
+            {
+                builder.Append(AllCharacters[Random.Shared.Next(0, AllCharacters.Count)]);
+            }
+
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Generates a random password with respect to the given rules.
+        /// </summary>
+        /// <param name="length">Length of the password.</param>
+        /// <param name="requireDigit">Whether the password requires a digit.</param>
+        /// <param name="requireLowercase">Whether the password requires a lowercase character.</param>
+        /// <param name="requireUppercase">Whether the password requires an uppercase character.</param>
+        /// <param name="requireNonAlphaNumeric">Whether the password requires a non-alphanumeric character</param>
+        /// <returns>Returns a random password.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static string GenerateWithCustomRules(int length, bool requireDigit, bool requireLowercase, bool requireUppercase, bool requireNonAlphaNumeric)
+        {
+            int leastNumberOfCharacters = 0;
+            if (requireDigit) ++leastNumberOfCharacters;
+            if (requireLowercase) ++leastNumberOfCharacters;
+            if (requireUppercase) ++leastNumberOfCharacters;
+            if (requireNonAlphaNumeric) ++leastNumberOfCharacters;
+            if (length < leastNumberOfCharacters)
+                throw new ArgumentException($"According to the rules, the least number of length should be {leastNumberOfCharacters}", nameof(length));
+
+            StringBuilder builder = new StringBuilder();
+            if (requireDigit) builder.Append(Digits[Random.Shared.Next(0, Digits.Count)]);
+            if (requireLowercase) builder.Append(LowercaseCharacters[Random.Shared.Next(0, LowercaseCharacters.Count)]);
+            if (requireUppercase) builder.Append(UppercaseCharacters[Random.Shared.Next(0, UppercaseCharacters.Count)]);
+            if (requireNonAlphaNumeric) builder.Append(NonAlphanumericCharacters[Random.Shared.Next(0, NonAlphanumericCharacters.Count)]);
+
+            length -= builder.Length;
+            for (int i = 0; i < length; ++i)
+            {
+                builder.Append(AllCharacters[Random.Shared.Next(0, AllCharacters.Count)]);
+            }
+
+            return builder.ToString();
+        }
+
+        private static List<char>? digits = null;
+        private static List<char> Digits
+        {
+            get
+            {
+                if (digits == null)
+                {
+                    digits = new List<char>();
+                    for (char i = '0'; i < '9'; ++i)
+                        digits.Add(i);
+                }
+
+                return digits;
+            }
+        }
+
+        private static List<char>? lowercaseCharacters = null;
+        private static List<char> LowercaseCharacters
+        {
+            get
+            {
+                if (lowercaseCharacters == null)
+                {
+                    lowercaseCharacters = new List<char>();
+                    for (char i = 'a'; i < 'z'; ++i)
+                        lowercaseCharacters.Add(i);
+                }
+
+                return lowercaseCharacters;
+            }
+        }
+
+        private static List<char>? uppercaseCharacters = null;
+        private static List<char> UppercaseCharacters
+        {
+            get
+            {
+                if (uppercaseCharacters == null)
+                {
+                    uppercaseCharacters = new List<char>();
+                    for (char i = 'A'; i < 'Z'; ++i)
+                        uppercaseCharacters.Add(i);
+                }
+
+                return uppercaseCharacters;
+            }
+        }
+
+        private static List<char>? nonAlphanumericCharacters = null;
+        private static List<char> NonAlphanumericCharacters
+        {
+            get
+            {
+                if (nonAlphanumericCharacters == null)
+                {
+                    nonAlphanumericCharacters = new List<char>()
+                    {
+                        ' ', '!', '"', '#', '$', '%', '&', '\'', '(',')', '*', '+', ',', '-', '.', '/', ':',
+                        ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'
+                    };
+                }
+
+                return nonAlphanumericCharacters;
+            }
+        }
+
+        private static List<char>? allCharacters = null;
+        private static List<char> AllCharacters
+        {
+            get
+            {
+                if (allCharacters == null)
+                {
+                    allCharacters = new List<char>();
+                    allCharacters.AddRange(Digits);
+                    allCharacters.AddRange(LowercaseCharacters);
+                    allCharacters.AddRange(UppercaseCharacters);
+                    allCharacters.AddRange(NonAlphanumericCharacters);
+                }
+
+                return allCharacters;
+            }
+        }
+    }
+}
---
+++ test/UnitTests/Cayd.Test.Generators.Test.Unit/Generators/PasswordGeneratorTest.cs
@@ -0,0 +1,68 @@
+﻿using System.Reflection;
+
+namespace Cayd.Test.Generators.Test.Unit.Generators
+{
+    public class PasswordGeneratorTest
+    {
+        [Fact]
+        public void Generate_WhenNoParameterIsGiven_ShouldReturnRandomPasswordWithLengthOfSix()
+        {
+            // Act
+            var result = PasswordGenerator.Generate();
+
+            // Assert
+            Assert.Equal(6, result.Length);
+        }
+
+        [Fact]
+        public void Generate_WhenLengthIsGiven_ShouldReturnRandomPasswordWithSameLength()
+        {
+            // Arrange
+            int length = 10;
+
+            // Act
+            var result = PasswordGenerator.Generate(length);
+
+            // Assert
+            Assert.Equal(length, result.Length);
+        }
+
+        [Fact]
+        public void GenerateWithCustomRules_WhenLengthIsEqualOrGreaterThanMinimumRequiredLength_ShouldReturnRandomPasswordWithSameLength()
+        {
+            // Arrange
+            var digits = (List<char>)(typeof(PasswordGenerator).GetProperty("Digits", BindingFlags.NonPublic | BindingFlags.Static))!.GetValue(null)!;
+            var lowercaseCharacters = (List<char>)(typeof(PasswordGenerator).GetProperty("LowercaseCharacters", BindingFlags.NonPublic | BindingFlags.Static))!.GetValue(null)!;
+            var uppercaseCharacters = (List<char>)(typeof(PasswordGenerator).GetProperty("UppercaseCharacters", BindingFlags.NonPublic | BindingFlags.Static))!.GetValue(null)!;
+            var nonAlphanumericCharacters = (List<char>)(typeof(PasswordGenerator).GetProperty("NonAlphanumericCharacters", BindingFlags.NonPublic | BindingFlags.Static))!.GetValue(null)!;
+            int length = 10;
+
+            // Act
+            var result = PasswordGenerator.GenerateWithCustomRules(length, true, true, true, true);
+
+            // Assert
+            Assert.Equal(length, result.Length);
+            Assert.True(digits.Any(d => result.Contains(d)));
+            Assert.True(lowercaseCharacters.Any(d => result.Contains(d)));
+            Assert.True(uppercaseCharacters.Any(d => result.Contains(d)));
+            Assert.True(nonAlphanumericCharacters.Any(d => result.Contains(d)));
+        }
+
+        [Fact]
+        public void GenerateWithCustomRules_WhenLengthIsLowerThanMinimumRequiredLength_ShouldThrowArgumentException()
+        {
+            // Arrange
+            int length = 3;
+
+            // Act
+            var result = Record.Exception(() =>
+            {
+                PasswordGenerator.GenerateWithCustomRules(length, true, true, true, true);
+            });
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<ArgumentException>(result);
+        }
+    }
+}
