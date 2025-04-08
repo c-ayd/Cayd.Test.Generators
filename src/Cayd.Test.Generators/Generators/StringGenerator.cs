c90d2bdaf3553ee1514e78769b0570f0b13415fe
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-08 14:08:53 +0200

    add string generator

---
+++ src/Cayd.Test.Generators/Generators/StringGenerator.cs
@@ -0,0 +1,143 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Cayd.Test.Generators
+{
+    public static class StringGenerator
+    {
+        /// <summary>
+        /// Generates a random string using Ascii characters.
+        /// </summary>
+        /// <param name="length">Length of the string.</param>
+        /// <returns>Returns a random string containing only Ascii characters.</returns>
+        public static string GenerateUsingAsciiChars(int length)
+            => GenerateString(AsciiChars, length);
+
+        /// <summary>
+        /// Generates a random string using Base64 URL characters.
+        /// </summary>
+        /// <param name="length">Length of the string.</param>
+        /// <returns>Returns a random string containing only Base64 URL characters.</returns>
+        public static string GenerateUsingBase64UrlChars(int length)
+            => GenerateString(Base64UrlChars, length);
+
+        /// <summary>
+        /// Generates a random string using custom characters.
+        /// </summary>
+        /// <param name="length">Length of the string.</param>
+        /// <returns>Returns a random string containing only the given characters.</returns>
+        public static string GenerateUsingCustomChars(List<char> chars, int length)
+            => GenerateString(chars, length);
+
+        /// <summary>
+        /// Generates a random string using Ascii characters with a format.
+        /// </summary>
+        /// <param name="format">Format to generate string. Example: "Hello, {0}. Welcome back!"</param>
+        /// <param name="parameterLengths">Length of the parameters in the format. Pass only one length if all parameters need to be the same length. Otherwise, the number of lengths must match with the number of the parameters.</param>
+        /// <returns>Returns a random string containing only Ascii characters.</returns>
+        public static string GenerateUsingAsciiCharsWithFormat(string format, params int[] parameterLengths)
+            => GenerateStringWithFormat(format, AsciiChars, parameterLengths);
+
+        /// <summary>
+        /// Generates a random string using Base64 URL characters with a format.
+        /// </summary>
+        /// <param name="format">Format to generate string. Example: "Hello, {0}. Welcome back!"</param>
+        /// <param name="parameterLengths">Length of the parameters in the format. Pass only one length if all parameters need to be the same length. Otherwise, the number of lengths must match with the number of the parameters.</param>
+        /// <returns>Returns a random string containing only Base64 URL characters.</returns>
+        public static string GenerateUsingBase64UrlCharsWithFormat(string format, params int[] parameterLengths)
+            => GenerateStringWithFormat(format, Base64UrlChars, parameterLengths);
+
+        /// <summary>
+        /// Generates a random string using custom characters with a format.
+        /// </summary>
+        /// <param name="format">Format to generate string. Example: "Hello, {0}. Welcome back!"</param>
+        /// <param name="parameterLengths">Length of the parameters in the format. Pass only one length if all parameters need to be the same length. Otherwise, the number of lengths must match with the number of the parameters.</param>
+        /// <returns>Returns a random string containing only the given characters.</returns>
+        public static string GenerateUsingCustomCharsWithFormat(string format, List<char> chars, params int[] parameterLengths)
+            => GenerateStringWithFormat(format, chars, parameterLengths);
+
+        private static string GenerateString(List<char> chars, int length)
+        {
+            if (chars == null || chars.Count == 0)
+                throw new ArgumentException("The character set is empty.", nameof(chars));
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                builder.Append(chars[Random.Shared.Next(0, chars.Count)]);
+            }
+
+            return builder.ToString();
+        }
+
+        private static string GenerateStringWithFormat(string format, List<char> chars, params int[] parameterLengths)
+        {
+            Regex regex = new Regex("{.*?}");
+            int numOfParameters = regex.Matches(format).Count;
+            if (numOfParameters == 0)
+                throw new ArgumentException($"There is no parameter in the format.", nameof(format));
+
+            if (parameterLengths.Count() == 0)
+                throw new ArgumentException("There must be at least one length element to generate strings.", nameof(parameterLengths));
+            if (parameterLengths.Length != 1 && parameterLengths.Length != numOfParameters)
+                throw new ArgumentException("There must be either one length element for all parameters in the format or the number of lengths must match with the number of the parameters.", nameof(parameterLengths));
+
+            List<string> parameters = new List<string>();
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < numOfParameters; i++)
+            {
+                int length = parameterLengths.Length == 1 ? parameterLengths[0] : parameterLengths[i];
+                for (int j = 0; j < length; j++)
+                {
+                    builder.Append(chars[Random.Shared.Next(0, chars.Count)]);
+                }
+
+                parameters.Add(builder.ToString());
+                builder.Clear();
+            }
+
+            return string.Format(format, parameters.ToArray());
+        }
+
+        private static List<char>? asciiChars = null;
+        private static List<char> AsciiChars
+        {
+            get
+            {
+                if (asciiChars == null)
+                {
+                    asciiChars = new List<char>();
+                    for (int i = 32; i <= 126; ++i)
+                    {
+                        asciiChars.Add((char)i);
+                    }
+                }
+
+                return asciiChars;
+            }
+        }
+
+        private static List<char>? base64UrlChars = null;
+        private static List<char> Base64UrlChars
+        {
+            get
+            {
+                if (base64UrlChars == null)
+                {
+                    base64UrlChars = new List<char>();
+                    for (char i = 'A'; i <= 'Z'; ++i)
+                        base64UrlChars.Add(i);
+                    for (char i = 'a'; i <= 'z'; ++i)
+                        base64UrlChars.Add(i);
+                    for (char i = '0'; i <= '9'; ++i)
+                        base64UrlChars.Add(i);
+
+                    base64UrlChars.Add('+');
+                    base64UrlChars.Add('=');
+                }
+
+                return base64UrlChars;
+            }
+        }
+    }
+}
---
+++ test/UnitTests/Cayd.Test.Generators.Test.Unit/Generators/StringGeneratorTest.cs
@@ -0,0 +1,156 @@
+﻿using System.Reflection;
+
+namespace Cayd.Test.Generators.Test.Unit.Generators
+{
+    public class StringGeneratorTest
+    {
+        [Fact]
+        public void GenerateString_WhenCharacterListIsGiven_ShouldGenerateStringWithOnlyThoseCharacters()
+        {
+            // Arrange
+            var method = typeof(StringGenerator).GetMethod("GenerateString", BindingFlags.NonPublic | BindingFlags.Static);
+            List<char> chars = new List<char>() { 'a', 'b', 'c', 'd', 'e' };
+
+            // Act
+            var result = (string)method!.Invoke(null, new object[] { chars, 5 })!;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.Length == 5, $"Length: {result.Length}, Expected: 5");
+            Assert.True(result.All(c => chars.Contains(c)), "Result contains characters that are not in the list.");
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(new char[] { })]
+        public void GenerateString_WhenCharacterListIsNullOrDoesNotContainAnyCharacter_ShouldThrowArgumentException(char[] charArray)
+        {
+            // Arrange
+            var method = typeof(StringGenerator).GetMethod("GenerateString", BindingFlags.NonPublic | BindingFlags.Static);
+            List<char>? chars = null;
+            if (charArray != null)
+            {
+                chars = new List<char>();
+                foreach (var item in chars)
+                {
+                    chars.Add(item);
+                }
+            }
+
+            // Act
+            var result = Record.Exception(() =>
+            {
+                method!.Invoke(null, new object[] { chars!, 5 });
+            });
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<ArgumentException>(result.InnerException);
+        }
+
+        [Fact]
+        public void GenerateStringWithFormat_WhenCharacterListAndFormatAndSameNumberOfLengthsAreGiven_ShouldGenerateStringWithOnlyThoseCharactersWithRelatedLengthsInSameFormat()
+        {
+            // Arrange
+            var method = typeof(StringGenerator).GetMethod("GenerateStringWithFormat", BindingFlags.NonPublic | BindingFlags.Static);
+            List<char> chars = new List<char>() { 'a', 'b', 'c', 'd', 'e' };
+            string format = "Hello -{0}-{1}- World";
+
+            // Act
+            var result = (string)method!.Invoke(null, new object[] { format, chars, new int[] { 5, 10 } })!;
+            var parts = result.Split('-');
+            var string1 = parts[1];
+            var string2 = parts[2];
+            var expected = string.Format(format, string1, string2);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(expected == result, $"Result: {result}, Expected: {expected}");
+            Assert.True(string1.Length == 5, $"String1 Length: {string1.Length}, Expected: 5");
+            Assert.True(string1.All(c => chars.Contains(c)), "String1 contains characters that are not in the list.");
+            Assert.True(string2.Length == 10, $"String2 Length: {string2.Length}, Expected: 10");
+            Assert.True(string2.All(c => chars.Contains(c)), "String2 contains characters that are not in the list.");
+        }
+
+        [Fact]
+        public void GenerateStringWithFormat_WhenCharacterListAndFormatAndOneLengthAreGiven_ShouldGenerateStringWithOnlyThoseCharactersWithSameLengthInSameFormat()
+        {
+            // Arrange
+            var method = typeof(StringGenerator).GetMethod("GenerateStringWithFormat", BindingFlags.NonPublic | BindingFlags.Static);
+            List<char> chars = new List<char>() { 'a', 'b', 'c', 'd', 'e' };
+            string format = "Hello -{0}-{1}- World";
+
+            // Act
+            var result = (string)method!.Invoke(null, new object[] { format, chars, new int[] { 5 } })!;
+            var parts = result.Split('-');
+            var string1 = parts[1];
+            var string2 = parts[2];
+            var expected = string.Format(format, string1, string2);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(expected == result, $"Result: {result}, Expected: {expected}");
+            Assert.True(string1.Length == 5, $"String1 Length: {string1.Length}, Expected: 5");
+            Assert.True(string1.All(c => chars.Contains(c)), "String1 contains characters that are not in the list.");
+            Assert.True(string2.Length == 5, $"String2 Length: {string2.Length}, Expected: 5");
+            Assert.True(string2.All(c => chars.Contains(c)), "String2 contains characters that are not in the list.");
+        }
+
+        [Fact]
+        public void GenerateStringWithFormat_WhenThereIsNoParameterInFormat_ShouldThrowArgumentException()
+        {
+            // Arrange
+            var method = typeof(StringGenerator).GetMethod("GenerateStringWithFormat", BindingFlags.NonPublic | BindingFlags.Static);
+            List<char> chars = new List<char>() { 'a', 'b', 'c', 'd', 'e' };
+            string format = "Hello World";
+
+            // Act
+            var result = Record.Exception(() =>
+            {
+                method!.Invoke(null, new object[] { format, chars, new int[] { 5 } });
+            });
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<ArgumentException>(result.InnerException);
+        }
+
+        [Fact]
+        public void GenerateStringWithFormat_WhenNoLengthForParametersIsGiven_ShouldThrowArgumentException()
+        {
+            // Arrange
+            var method = typeof(StringGenerator).GetMethod("GenerateStringWithFormat", BindingFlags.NonPublic | BindingFlags.Static);
+            List<char> chars = new List<char>() { 'a', 'b', 'c', 'd', 'e' };
+            string format = "Hello -{0}-{1}- World";
+
+            // Act
+            var result = Record.Exception(() =>
+            {
+                method!.Invoke(null, new object[] { format, chars, new int[] { } });
+            });
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<ArgumentException>(result.InnerException);
+        }
+
+        [Fact]
+        public void GenerateStringWithFormat_WhenNumberOfLengthsForParametersIsNotOneAndDoesNotMatchWithLengthOfParameters_ShouldThrowArgumentException()
+        {
+            // Arrange
+            var method = typeof(StringGenerator).GetMethod("GenerateStringWithFormat", BindingFlags.NonPublic | BindingFlags.Static);
+            List<char> chars = new List<char>() { 'a', 'b', 'c', 'd', 'e' };
+            string format = "Hello -{0}-{1}- World";
+
+            // Act
+            var result = Record.Exception(() =>
+            {
+                method!.Invoke(null, new object[] { format, chars, new int[] { 5, 10, 15 } });
+            });
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<ArgumentException>(result.InnerException);
+        }
+    }
+}
