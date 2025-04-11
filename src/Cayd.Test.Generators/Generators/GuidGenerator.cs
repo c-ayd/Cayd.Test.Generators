df1809e362b1f926093de1cbf847d16985bbe4a8
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-11 19:58:08 +0200

    add guid generator

---
+++ src/Cayd.Test.Generators/Generators/GuidGenerator.cs
@@ -0,0 +1,29 @@
+﻿namespace Cayd.Test.Generators
+{
+    public static class GuidGenerator
+    {
+        /// <summary>
+        /// Generates a Guid.
+        /// </summary>
+        /// <returns>Returns a Guid.</returns>
+        public static Guid Generate()
+            => Guid.NewGuid();
+
+        /// <summary>
+        /// Generates a basic time-based sequential Guid.
+        /// </summary>
+        /// <returns>Returns a sequential Guid.</returns>
+        public static Guid GenerateSequential()
+        {
+            byte[] dateTimeBytes = BitConverter.GetBytes(DateTime.UtcNow.Ticks);
+            byte[] randomBytes = new byte[8];
+            Random.Shared.NextBytes(randomBytes);
+
+            byte[] guidBytes = new byte[16];
+            Array.Copy(dateTimeBytes, 0, guidBytes, 0, 8);
+            Array.Copy(randomBytes, 0, guidBytes, 8, 8);
+
+            return new Guid(guidBytes);
+        }
+    }
+}
---
+++ test/UnitTests/Cayd.Test.Generators.Test.Unit/Generators/GuidGeneratorTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Cayd.Test.Generators.Test.Unit.Generators
+{
+    public class GuidGeneratorTest
+    {
+        [Fact]
+        public void GenerateSequential_ShouldGenerateSequentialGuids()
+        {
+            // Act
+            var result1 = GuidGenerator.GenerateSequential();
+            var result2 = GuidGenerator.GenerateSequential();
+            var result3 = GuidGenerator.GenerateSequential();
+
+            // Assert
+            Assert.True(result1.CompareTo(result2) < 0, "Guid1 and 2 are not sequential");
+            Assert.True(result2.CompareTo(result3) < 0, "Guid2 and 3 are not sequential");
+        }
+    }
+}
