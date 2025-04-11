7a9fbc39ecb2af945475e453e324985febe735eb
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-11 19:19:33 +0200

    add time span generator

---
+++ src/Cayd.Test.Generators/Generators/TimeSpanGenerator.cs
@@ -0,0 +1,88 @@
+﻿namespace Cayd.Test.Generators
+{
+    public static class TimeSpanGenerator
+    {
+        /// <summary>
+        /// Generates a random time span between -7 and 7 days, including hours, minutes, seconds and milliseconds.
+        /// </summary>
+        /// <returns>Returns a random time span.</returns>
+        public static TimeSpan Generate()
+        {
+            var timeSpan = new TimeSpan(
+                Random.Shared.Next(0, 7),
+                Random.Shared.Next(0, 24),
+                Random.Shared.Next(0, 60),
+                Random.Shared.Next(0, 60),
+                Random.Shared.Next(1, 1000));
+
+            return BooleanGenerator.Generate() ? timeSpan : -timeSpan;
+        }
+
+        /// <summary>
+        /// Generates a random time span between -7 and 7 days, including hours, minutes, seconds and milliseconds, based on a time direction.
+        /// </summary>
+        /// <param name="timeDirection">Direction of the time span. <see cref="ETimeDirection.Positive"/> indicates future, whereas <see cref="ETimeDirection.Negative"/> indicates past.</param>
+        /// <returns>Returns a random time span.</returns>
+        public static TimeSpan Generate(ETimeDirection timeDirection)
+        {
+            var timeSpan = new TimeSpan(
+                Random.Shared.Next(0, 7),
+                Random.Shared.Next(0, 24),
+                Random.Shared.Next(0, 60),
+                Random.Shared.Next(0, 60),
+                Random.Shared.Next(1, 1000));
+
+            return timeDirection == ETimeDirection.Positive ? timeSpan : -timeSpan;
+        }
+
+        /// <summary>
+        /// Generates a random time span based on a time limit including days, hours, minutes, seconds and milliseconds.
+        /// </summary>
+        /// <param name="timeLimit">Limit of the time span. If a limit is not specified for a specific time unit, the time unit is always 0.</param>
+        /// <returns>Returns a random time span.</returns>
+        public static TimeSpan GenerateWithLimit(TimeLimit timeLimit)
+        {
+            var timeSpan = new TimeSpan(
+                Random.Shared.Next(0, timeLimit.DayLimit + 1),
+                Random.Shared.Next(0, timeLimit.HourLimit + 1),
+                Random.Shared.Next(0, timeLimit.MinuteLimit + 1),
+                Random.Shared.Next(0, timeLimit.SecondLimit + 1),
+                Random.Shared.Next(0, timeLimit.MillisecondLimit + 1));
+
+            return BooleanGenerator.Generate() ? timeSpan : -timeSpan;
+        }
+
+        /// <summary>
+        /// Generates a random time span based on a time direction and a time limit including days, hours, minutes, seconds and milliseconds
+        /// </summary>
+        /// <param name="timeDirection">Direction of the time span. <see cref="ETimeDirection.Positive"/> indicates future, whereas <see cref="ETimeDirection.Negative"/> indicates past.</param>
+        /// <param name="timeLimit">Limit of the time span. If a limit is not specified for a specific time unit, the time unit is always 0.</param>
+        /// <returns>Returns a random time span.</returns>
+        public static TimeSpan GenerateWithLimit(ETimeDirection timeDirection, TimeLimit timeLimit)
+        {
+            var timeSpan = new TimeSpan(
+               Random.Shared.Next(0, timeLimit.DayLimit + 1),
+               Random.Shared.Next(0, timeLimit.HourLimit + 1),
+               Random.Shared.Next(0, timeLimit.MinuteLimit + 1),
+               Random.Shared.Next(0, timeLimit.SecondLimit + 1),
+               Random.Shared.Next(0, timeLimit.MillisecondLimit + 1));
+
+            return timeDirection == ETimeDirection.Positive ? timeSpan : -timeSpan;
+        }
+
+        public enum ETimeDirection
+        {
+            Positive        =   0,
+            Negative        =   1
+        }
+
+        public struct TimeLimit
+        {
+            public int DayLimit { get; set; }
+            public int HourLimit { get; set; }
+            public int MinuteLimit { get; set; }
+            public int SecondLimit { get; set; }
+            public int MillisecondLimit { get; set; }
+        }
+    }
+}
---
+++ test/UnitTests/Cayd.Test.Generators.Test.Unit/Generators/TimeSpanGeneratorTest.cs
@@ -0,0 +1,83 @@
+﻿namespace Cayd.Test.Generators.Test.Unit.Generators
+{
+    public class TimeSpanGeneratorTest
+    {
+        [Fact]
+        public void Generate_WhenTimeDirectionIsNotGiven_ShouldGenerateTimeSpanRatherThanZero()
+        {
+            // Act
+            var result = TimeSpanGenerator.Generate();
+
+            // Assert
+            Assert.NotEqual(TimeSpan.Zero, result);
+        }
+
+        [Theory]
+        [InlineData(TimeSpanGenerator.ETimeDirection.Positive)]
+        [InlineData(TimeSpanGenerator.ETimeDirection.Negative)]
+        public void Generate_WhenTimeDirectionIsGiven_ShouldGenerateTimeSpanRatherThanZero(TimeSpanGenerator.ETimeDirection timeDirection)
+        {
+            // Act
+            var result = TimeSpanGenerator.Generate(timeDirection);
+
+            // Assert
+            Assert.NotEqual(TimeSpan.Zero, result);
+            Assert.True(timeDirection == TimeSpanGenerator.ETimeDirection.Positive ? 
+                (result.TotalSeconds > 0) : (result.TotalSeconds < 0), $"Time Direction: {timeDirection.ToString()}");
+        }
+
+        [Fact]
+        public void GenerateWithLimit_WhenTimeDirectionIsNotGiven_ShouldGenerateTimeSpanWithinRange()
+        {
+            // Assert
+            var timeLimit = new TimeSpanGenerator.TimeLimit()
+            {
+                DayLimit = 5,
+                HourLimit = 10,
+                MinuteLimit = 0,
+                SecondLimit = 20,
+                MillisecondLimit = 30
+            };
+
+            // Act
+            var result = TimeSpanGenerator.GenerateWithLimit(timeLimit);
+
+            // Arrange
+            Assert.True(result.Days >= -5 && result.Days <= 5, $"Days: {result.Days}, Expected: [-5, 5]");
+            Assert.True(result.Hours >= -10 && result.Hours <= 10, $"Hours: {result.Hours}, Expected: [-10, 10]");
+            Assert.True(result.Minutes == 0, $"Minutes: {result.Minutes}, Expected: 0");
+            Assert.True(result.Seconds >= -20 && result.Seconds <= 20, $"Seconds: {result.Seconds}, Expected: [-20, 20]");
+            Assert.True(result.Milliseconds >= -30 && result.Milliseconds <= 30, $"Milliseconds: {result.Milliseconds}, Expected: [-30, 30]");
+        }
+
+        [Theory]
+        [InlineData(TimeSpanGenerator.ETimeDirection.Positive)]
+        [InlineData(TimeSpanGenerator.ETimeDirection.Negative)]
+        public void GenerateWithLimit_WhenTimeDirectionIsGiven_ShouldGenerateTimeSpanWithinRange(TimeSpanGenerator.ETimeDirection timeDirection)
+        {
+            // Assert
+            var timeLimit = new TimeSpanGenerator.TimeLimit()
+            {
+                DayLimit = 5,
+                HourLimit = 10,
+                MinuteLimit = 0,
+                SecondLimit = 20,
+                MillisecondLimit = 30
+            };
+
+            // Act
+            var result = TimeSpanGenerator.GenerateWithLimit(timeDirection, timeLimit);
+
+            // Arrange
+            Assert.True(timeDirection == TimeSpanGenerator.ETimeDirection.Negative ? 
+                result.Days >= -5 : result.Days <= 5, $"Days: {result.Days}, Direction: {timeDirection.ToString()}");
+            Assert.True(timeDirection == TimeSpanGenerator.ETimeDirection.Negative ? 
+                result.Hours >= -10 : result.Hours <= 10, $"Hours: {result.Hours}, Direction: {timeDirection.ToString()}");
+            Assert.True(result.Minutes == 0, $"Minutes: {result.Minutes}, Expected: 0");
+            Assert.True(timeDirection == TimeSpanGenerator.ETimeDirection.Negative ? 
+                result.Seconds >= -20 : result.Seconds <= 20, $"Seconds: {result.Seconds}, Direction: {timeDirection.ToString()}");
+            Assert.True(timeDirection == TimeSpanGenerator.ETimeDirection.Negative ? 
+                result.Milliseconds >= -30 : result.Milliseconds <= 30, $"Milliseconds: {result.Milliseconds}, Direction: {timeDirection.ToString()}");
+        }
+    }
+}
