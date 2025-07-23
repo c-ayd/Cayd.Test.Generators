a2ebb4ea7778a2414d253d51ca1d47970a3c062d
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-23 19:28:20 +0200

    add mac address generator

---
+++ src/Cayd.Test.Generators/Generators/MacAddressGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+
+namespace Cayd.Test.Generators.Generators
+{
+    public static class MacAddressGenerator
+    {
+        /// <summary>
+        /// Read only MAC address whose value is 'FF:FF:FF:FF:FF:FF'
+        /// </summary>
+        public static readonly string Broadcast = "FF:FF:FF:FF:FF:FF";
+
+        /// <summary>
+        /// Generates a unicast MAC address.
+        /// </summary>
+        /// <returns>Returns a unicast MAC address.</returns>
+        public static string GenerateUnicast()
+            => GenerateMacAddress(0x00);
+
+        /// <summary>
+        /// Generates a multicast MAC address.
+        /// </summary>
+        /// <returns>Returns a multicast MAC address.</returns>
+        public static string GenerateMulticast()
+            => GenerateMacAddress(0x01);
+
+        private static string GenerateMacAddress(byte type)
+        {
+            var bytes = new byte[6];
+            RandomNumberGenerator.Fill(bytes);
+
+            bytes[0] = (byte)(type | (bytes[0] & 0xFE));
+
+            return BitConverter.ToString(bytes).Replace('-', ':');
+        }
+    }
+}
---
+++ test/UnitTests/Cayd.Test.Generators.Test.Unit/Generators/MacAddressGeneratorTest.cs
@@ -0,0 +1,35 @@
+﻿using Cayd.Test.Generators.Generators;
+
+namespace Cayd.Test.Generators.Test.Unit.Generators
+{
+    public class MacAddressGeneratorTest
+    {
+        [Fact]
+        public void GenerateUnicast_ShouldGenerateUnicastMacAddress()
+        {
+            // Act
+            var result = MacAddressGenerator.GenerateUnicast();
+
+            // Arrange
+            Assert.Equal(17, result.Length);
+
+            var firstPart = result.Split(':')[0];
+            var firstByte = Convert.ToByte(firstPart, fromBase: 16);
+            Assert.True((firstByte & 0x00) == 0);
+        }
+
+        [Fact]
+        public void GenerateMulticast_ShouldGenerateMulticastMacAddress()
+        {
+            // Act
+            var result = MacAddressGenerator.GenerateMulticast();
+
+            // Arrange
+            Assert.Equal(17, result.Length);
+
+            var firstPart = result.Split(':')[0];
+            var firstByte = Convert.ToByte(firstPart, fromBase: 16);
+            Assert.True((firstByte & 0x01) == 1);
+        }
+    }
+}
